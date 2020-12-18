7e0eb445c489d663e3f4fd62b7029579bf16df7b
enrico.carboniero <enrico.carboniero@gmail.com>
2020-12-18 08:23:46 +0100

    Add logger.

--- api/src/main/java/it/labtech/base/api/impl/DefaultTestImpl.java
+++ api/src/main/java/it/labtech/base/api/impl/DefaultTestImpl.java
@@ -1,13 +1,21 @@
 package it.labtech.base.api.impl;
 
 import it.labtech.base.api.endpoint.ITest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class DefaultTestImpl implements ITest {
 
+    private static final Logger _LOGGER = LoggerFactory.getLogger(DefaultTestImpl.class);
+
     @Override
     public String getValue() {
-        return "Hello World :-D";
+        _LOGGER.debug("Start getValue()");
+        var retValue = "Hello World :-D";
+        _LOGGER.debug("End getValue()");
+        return retValue;
     }
 }
