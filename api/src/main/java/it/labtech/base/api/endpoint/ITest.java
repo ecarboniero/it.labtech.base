3914b972b2daf3bcde608265966af421589e3568
ecarboniero <enrico.carboniero@gmail.com>
2021-01-04 10:43:58 +0000

    Fix springdoc-openapi-ui dep and ITest endpoint.

--- api/src/main/java/it/labtech/base/api/endpoint/ITest.java
+++ api/src/main/java/it/labtech/base/api/endpoint/ITest.java
@@ -12,7 +12,6 @@ public interface ITest {
     @GetMapping
     String getValue();
 
-    @GetMapping
-    @RequestMapping(value = "/service")
+    @GetMapping(value = "/service")
     List<Service> getServices();
 }
